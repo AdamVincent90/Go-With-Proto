bc4186e02787c49a01f02ddc662b39a28e8baede
Adam Vincent <adam.vincent@totalprocessing.com>
2021-04-08 13:42:31 +0100

    change script comment

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,2 +1,2 @@
-# protoc command | generate code to "." (this folder) | generate proto files from this folder (proto/*.proto) * representing all
+# protoc command | generate code to "src/" (src folder) | generate proto files from this folder (proto/*.proto) * representing all
 protoc --go_out=src/ proto/*.proto
